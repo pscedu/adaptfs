d97143aa6c11cfa23e7fd961bca7fc9ce07d208f
Anjana Kar <kar@axon.psc.edu>
2017-09-12 12:52:53 -0400

    fix bounds checks

--- mount_adaptfs/ctl.c
+++ mount_adaptfs/ctl.c
@@ -41,20 +41,22 @@ ctlcmd_load(int fd, struct psc_ctlmsghdr *mh, void *m)
 	int i;
 
 	if (l->nargs < 0 || l->nargs > NARGS_MAX)
-		return (psc_ctlsenderr(fd, mh,
+		return (psc_ctlsenderr(fd, mh, NULL,
 		    "invalid number of arguments"));
 
 	argnames = PSCALLOC(sizeof(char *) * l->nargs);
 	argvals = PSCALLOC(sizeof(char *) * l->nargs);
 
 	for (i = 0; i < l->nargs; i++) {
+		printf("recv arg %i %s = %s\n", i, l->argnames[i], l->argvals[i]);
 		argnames[i] = pfl_strdup(l->argnames[i]);
 		argvals[i] = pfl_strdup(l->argvals[i]);
 	}
 
 	if (instance_load(l->name, l->module, argnames, argvals,
 	    l->nargs) == NULL)
-		return (psc_ctlsenderr(fd, mh, "instance failed to load"));
+		return (psc_ctlsenderr(fd, mh, NULL,
+		    "instance failed to load"));
 	return (1);
 }
 
@@ -63,15 +65,11 @@ struct psc_ctlop ctlops[] = {
 	{ ctlcmd_load,	sizeof(struct ctlmsg_load) }
 };
 
-psc_ctl_thrget_t psc_ctl_thrgets[] = {
-};
-
-PFLCTL_SVR_DEFS;
-
 void
 ctlthr_main(__unusedx struct psc_thread *thr)
 {
-	psc_ctlthr_main(ctlsockfn, ctlops, nitems(ctlops), THRT_CTLAC);
+	psc_ctlthr_main(ctlsockfn, ctlops, nitems(ctlops), 0,
+	    THRT_CTLAC);
 }
 
 void
@@ -94,7 +92,7 @@ ctlthr_spawn(void)
 //	psc_ctlparam_register_simple("version", ctlparam_version_get,
 //	    NULL);
 
-	thr = pscthr_init(THRT_CTL, ctlthr_main, NULL,
+	thr = pscthr_init(THRT_CTL, ctlthr_main, 
 	    sizeof(struct psc_ctlthr), "ctlthr0");
 	pscthr_setready(thr);
 }
